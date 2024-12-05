6323da21685604e97265de7f53d7344ba471f953
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-12-05 23:57:23 +0100

    totally finished

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,2 +1,2 @@
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001']
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', "https://directsport.vercel.app"]
 module.exports = allowedOrigins;
